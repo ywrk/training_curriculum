d25d2750aa06566959cba811e7fdb7f03aed3814
mamoo3 <mamoo.wprg@gmail.com>
2020-11-03 12:21:17 +0900

    Issue6の解消（修正）
    カレンダーの画面をキャプチャをアップします。
    
    https://gyazo.com/6178c7fc3b9577df45f43f35ccbcc984

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,9 +34,9 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = Date.today.wday
+      wday_num = Date.today.wday + x
       if wday_num >= 7
-        wday_num = wday_num - 7
+         wday_num = wday_num - 7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
